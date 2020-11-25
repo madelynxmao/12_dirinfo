1aaf79084a7c7eecf78aa1c3c69e03d378127011
madelynxmao <mmao10@stuy.edu>
2020-11-24 21:07:54 -0500

    updated main

--- main.c
+++ main.c
@@ -3,20 +3,43 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <errno.h>
 
 /*
 Madelyn Mao
 Systems Level Programming
 HW12 -- Please follow the Dir[ections]
 2020-11-22
+
+sample run: ./main test
+OR
+
+./main
+test (when prompted)
 */
 
-int main(){
+int main(int argc, char **argv){
+    char directory_to_scan[80];
 
-    //Have the program scan the current directory
+    char backup[80]; //bc stdin makes the string end in \n and not \0
+
+    if (argc == 2){
+        strncpy(directory_to_scan, argv[1], sizeof(directory_to_scan)- 1);
+    }
+    else{
+        printf("Enter a directory to scan: ");
+        fgets(backup, sizeof(backup), stdin);
+        strncpy(directory_to_scan,backup,sizeof(directory_to_scan)-1);
+    }
+    //Have the program scan the directory
 
     DIR *directory;
-    directory = opendir(".");
+    directory = opendir(directory_to_scan);
+
+    if (!directory){
+        printf("Error: %s\n", strerror(errno));
+    }
 
     struct dirent *entry;
     entry = readdir(directory);
